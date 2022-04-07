d03cd099c0593d3d13eab93d61b809da708e7daf
Schneider <jncxnsee@tuta.io>
2022-04-07 12:13:20 +0500

    Fixes, merged all scripts in one file. Fixed name collison of swiper sliders

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+const secondSwiper = new Swiper(".swiperSlider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+// smooth scrolling
 (function () {
   
     // Scroll Variables (tweakable)
@@ -771,4 +801,4 @@
     else
         window.SmoothScroll = SmoothScroll;
     
-    })();
+})();
