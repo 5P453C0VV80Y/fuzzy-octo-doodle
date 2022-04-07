8a78338d29b845687c6af7e5a391e35cee46fdec
Schneider <jncxnsee@tuta.io>
2022-04-07 12:27:33 +0500

    swiper issues

--- main.js
+++ main.js
@@ -8,24 +8,6 @@ const swiper = new Swiper(".mySwiper", {
   },
 });
 
-const secondSwiper = new Swiper(".swiperSlider", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-
 
 // smooth scrolling
 (function () {
